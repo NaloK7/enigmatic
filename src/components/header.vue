e1b6ab9760d8a26f984705b6caad8bc21271b931
NaloK <129076162+NaloK7@users.noreply.github.com>
2024-07-15 20:07:48 +0200

    header with tailwind

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,102 +1,39 @@
 <template>
   <header>
-    <div class="flex glass-header" id="top-header">
-      <div>
+    <div
+      class="flex justify-between items-center dark-glass py-6 px-10 shadow-pinkShadow relative z-10 [&>*]:w-1/5"
+      id="top-header">
+      <div class="w-1/5">
         <a class="logo-img" href="#">
           <img src="/logo-img.svg" alt="logo" />
         </a>
       </div>
-      <div class="line"></div>
-      <a href="#" id="logo-title" class="font-audiowide font-bold text-2xl"
+      <div class="w-1/5 h-px bg-primaryGreen"></div>
+      <a
+        href="#"
+        class="text-center font-audiowide font-bold text-2xl px-4 min-w-80"
         >Enigmatic Journey
       </a>
-      <div class="line"></div>
-      <div>
+      <div class="w-1/5 h-px bg-primaryGreen"></div>
+
+      <div class="w-1/5 flex justify-end">
         <img src="./img/account-icon.svg" alt="menu icon" id="icon-account" />
       </div>
     </div>
-    <div class="glass-header" id="bottom-header">
-      <nav>
-        <a href="#" class="book-btn">Livre I</a>
-        <a href="#" class="book-btn">Livre II</a>
-        <a href="#" class="book-btn">Livre III</a>
-        <a href="#" class="book-btn">Livre IV</a>
+  
+      <nav class="dark-glass shadow-pinkShadow mx-auto w-3/5 flex items-center justify-evenly mb-5 rounded-b-xl relative backdrop-blur-md ">
+        <a href="#">Livre I</a>
+        <a href="#">Livre II</a>
+        <a href="#">Livre III</a>
+        <a href="#">Livre IV</a>
       </nav>
-    </div>
+    
   </header>
 </template>
 
 <style scoped>
-.line {
-  height: 1px;
-  width: 100%;
-  background-color: #17ae9f;
-}
-
-#icon-account {
-  width: 26px;
-}
-.glass-header {
-  background: linear-gradient(
-    90deg,
-    rgba(0, 0, 0, 0.1) 0%,
-    rgba(0, 0, 0, 0.7) 40%,
-    rgba(0, 0, 0, 0.7) 60%,
-    rgba(0, 0, 0, 0.1) 100%
-  );
-
-  backdrop-filter: blur(10px);
-}
-
-div#top-header div {
-  width: 20%;
-}
-
-#top-header {
-  padding: 25px 40px;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0px 4px 15px var(--main-pink), 0px 2px 11px #535353;
-  display: flex;
-  position: relative;
-  z-index: 2;
-}
-
-/* #top-header > div:nth-child(3) {
-  display: flex;
-  justify-content: end;
-} */
-#top-header > div:nth-child(5) {
-  display: flex;
-  justify-content: end;
-}
-#bottom-header {
-  width: 60%;
-  border-radius: 0px 0px 10px 10px;
-  margin: 0 auto;
-  justify-content: center;
-  box-shadow: 0px 4px 15px var(--main-pink);
-  position: relative;
-  z-index: 1;
-  backdrop-filter: blur(10px);
-  top: -46px;
-  transition: 0.5s;
-  transition-delay: 1200ms;
-}
-#bottom-header:hover {
-  transition: 0.5s;
-  top: 0px;
-}
-
-nav {
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  margin-bottom: 20px;
-}
-
 nav a {
-  margin: 20px;
+  margin: 22px;
   height: 28px;
   width: 92px;
   border: 2px solid #17ae9f;
@@ -107,4 +44,17 @@ nav a {
   text-align: center;
   line-height: 1.4em;
 }
+#icon-account {
+  width: 26px;
+}
+nav {
+  top: -50px;
+  transition: 0.5s;
+  transition-delay: 1000ms;
+}
+nav:hover {
+  transition: 0.5s;
+  top: 0px;
+}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ export default {
         audiowide: ["Audiowide", "sans-serif"],
       },
     },
+    colors: {
+      primaryGreen: "#17ae9f",
+      primaryPink: "#ff4281",
+    },
+    boxShadow: {
+      'pinkShadow': '0px 4px 15px #ff0f5f, 0px 2px 11px #535353',
+      'greenShadow': '0px 4px 15px #17ae9f, 0px 2px 11px #535353',
+    },
   },
   plugins: [],
 };
