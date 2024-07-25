ec415b43f96118cfabe589bdb129fb67d7c7534a
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-07-25 10:27:57 +0200

    add Access control index.php

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require_once('./controller/UserController.php');
 try {
     // route
