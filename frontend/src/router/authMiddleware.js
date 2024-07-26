d50a27c8fbc8d2dc10fef8f1ffb5503c942080f4
NaloK <129076162+NaloK7@users.noreply.github.com>
2024-07-26 21:46:45 +0200

    middleware review

--- frontend/src/router/authMiddleware.js
+++ frontend/src/router/authMiddleware.js
@@ -3,31 +3,45 @@ import { jwtDecode } from "jwt-decode";
 // MIDDLEWARE
 const authMiddleware = (to, from, next) => {
   const token = localStorage.getItem("token");
+  // TOKEN
   if (
     token !== null &&
     token !== undefined &&
     token !== "" &&
     token !== "undefined"
   ) {
-    console.log("1");
+    console.log("token");
     const decoded = jwtDecode(token);
     const expiration = new Date(decoded.exp * 1000);
 
     if (expiration > new Date()) {
-      console.log("2");
+      // TOKEN VALID
+      console.log("token valid");
       next();
+      return;
     } else {
-      console.log("3");
-      // localStorage.removeItem("token");
+      // TOKEN INVALID
+      console.log("token expired");
+      localStorage.removeItem("token");
       next({ name: "login" });
+      return;
     }
-  }
-  if (to.name !== "login" && to.name !== "inscription" && to.name !== "home") {
-    console.log("4");
-    // localStorage.removeItem("token");
-    next({ name: "home" });
+    // NO TOKEN
   } else {
-    next();
+    if (
+      to.name !== "login" &&
+      to.name !== "inscription" &&
+      to.name !== "home"
+    ) {
+      console.log("no token");
+      localStorage.removeItem("token");
+      next({ name: "home" });
+      return;
+    } else {
+      console.log("no token: to login/inscription/home");
+      next();
+      return;
+    }
   }
 };
 
