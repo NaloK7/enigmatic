21a7339da1f283e4b8c7295af294c5552eaf00ad
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-08-23 15:02:57 +0200

    navBtn feedback if section is finished

--- frontend/src/components/navBtn.vue
+++ frontend/src/components/navBtn.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    :class="{ 'border-primaryPink hover:border-secondaryPink': finished }"
     class="block w-1/4 md:min-w-24 h-8 md:my-5 md:h-7 md:w-24 border md:border-2 border-primaryGreen sm:before:content-['Livre\00a0'] md:rounded-md bg-secondaryGreen text-white font-audiowide hover:text-black hover:bg-primaryGreen hover:border-secondaryGreen"
     @click="redirectToLast(section)">
     {{ text }}
@@ -10,12 +11,14 @@
 import api from "@/composables/api";
 
 import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 const router = useRouter();
 const props = defineProps({
   text: String,
   section: String,
 });
+
+const finished = ref(false);
 
 async function redirectToLast(bookId) {
   const response = await api.getLast(bookId);
@@ -30,4 +33,9 @@ async function redirectToLast(bookId) {
     console.log(response.status);
   }
 }
+
+onMounted(async () => {
+  const xhrFinish = await api.isFinished(props.section);
+  finished.value = xhrFinish.data;
+});
 </script>
